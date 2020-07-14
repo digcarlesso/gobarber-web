a4f098adef54d91f9ea4078799c90c5e6057edae
Diogo Bedin Carlesso <dig.skull@gmail.com>
2020-07-13 22:28:55 -0300

    Implemented Routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
-import { userInfo } from 'os';
+// import { userInfo } from 'os';
 import api from '../services/api';
 
 interface AuthState {
